7d41675fd64246727c2b2eee9ae6bac8240d5a53
Nester <nesternash2004@gmail.com>
2025-03-16 20:31:28 +0530

    fixed changes

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -2,6 +2,7 @@ import { Button } from '@/components/ui/button'
 import React from 'react'
 import Authentication from './Authentication'
 import Link from 'next/link'
+import { GitBranchIcon } from 'lucide-react'
 
 const Hero = () => {
   return (
@@ -10,7 +11,7 @@ const Hero = () => {
         <p className='mt-2 w-full md:w-1/2 text-center opacity-50'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, cum architecto, quibusdam explicabo fuga laudantium pariatur aliquid sequi ea officiis repellat accusantium nisi commodi sed, nesciunt illum dignissimos nihil. Quo.</p>
         <div className='flex gap-3 mt-3'>
           <Link href={"https://github.com/N3sterFerns"}>
-            <Button size={"lg"} variant={"secondary"}>Github</Button>
+            <Button size={"lg"} variant={"secondary"}><GitBranchIcon/> N3sterFerns</Button>
           </Link>
             <Authentication>
                 <Button className={"cursor-pointer"} size={"lg"}>Get Started</Button>
